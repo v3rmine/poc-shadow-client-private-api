f72d4ef8e027e6c948dac633b3d64ca170fe844c
Johan Planchon <dev@planchon.xyz>
2020-02-17 14:23:56 +0100

    Documentation

---
+++ docs/implementors/serde/ser/trait.Serialize.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["shadowtech_api"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.104/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"shadowtech_api/log/struct.LogQuery.html\" title=\"struct shadowtech_api::log::LogQuery\">LogQuery</a>","synthetic":false,"types":["shadowtech_api::log::LogQuery"]},{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.104/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"shadowtech_api/log/struct.LogQueryMetadata.html\" title=\"struct shadowtech_api::log::LogQueryMetadata\">LogQueryMetadata</a>","synthetic":false,"types":["shadowtech_api::log::LogQueryMetadata"]},{"text":"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.104/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"shadowtech_api/log/struct.LogQueryValues.html\" title=\"struct shadowtech_api::log::LogQueryValues\">LogQueryValues</a>","synthetic":false,"types":["shadowtech_api::log::LogQueryValues"]}];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
---
+++ docs/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["shadowtech_api"] = {"name":"","dirs":[{"name":"auth","files":["mod.rs"]},{"name":"computer","files":["mod.rs"]},{"name":"customer","files":["mod.rs"]},{"name":"dispatcher","files":["mod.rs"]},{"name":"general","files":["mod.rs"]},{"name":"log","files":["mod.rs"]},{"name":"status","files":["mod.rs"]}],"files":["client.rs","constants.rs","errors.rs","lib.rs","macros.rs"]};
+createSourceSidebar();
